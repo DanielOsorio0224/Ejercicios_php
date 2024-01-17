1c0fce84a0f30ede1ac1dd2c977fda513bf08133
danielosorio0224 <dosorioquimbayo@gmail.com>
2024-01-17 16:38:15 -0500

    Ejercicio-1

---
+++ ejercicio1.php
@@ -0,0 +1,15 @@
+<center>
+<?php
+//Realizar el clasico triangulo de astericos
+$n = 5;
+//
+echo "TRIANGULO"."<br/>";
+for($i = 1; $i <= $n ;$i++){    
+    for($j = 1; $j <= $i; $j++){
+        echo "* ";        
+    }
+     echo "<br/>";     
+}
+
+?>
+</center>
